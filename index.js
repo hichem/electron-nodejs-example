5f7507c59bc3b2007d277541db0c310651da98de
Hichem BOUSSETTA <hichem.boussetta@ratpsmartsystems.com>
2020-09-29 10:42:46 +0200

    [TPV-470] added node-ipc and sample ipc client example

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const ipc=require('node-ipc');
 
 function createWindow () {
   // Cree la fenetre du navigateur.
@@ -35,3 +36,87 @@ app.on('window-all-closed', () => {
       createWindow()
     }
   })
+
+
+  //function startClient() {
+  /***************************************\
+   *
+   * You should start both hello and world
+   * then you will see them communicating.
+   *
+   * *************************************/
+
+  // ipc.config.socketRoot = 'C:\\tmp\\';
+  // ipc.config.appspace = 'app.';
+  ipc.config.id = 'hello';
+  ipc.config.retry= 1500;
+  ipc.config.rawBuffer=true;
+  ipc.config.encoding='ascii';
+  ipc.log('## ipc configured ##');
+
+  ipc.connectTo(
+      'world',
+      function(){
+          ipc.of.world.on(
+              'connect',
+              function(){
+                  ipc.log('## connected to world ##', ipc.config.delay);
+                  ipc.of.world.emit(
+                      'hello'
+                  );
+              }
+          );
+
+          ipc.of.world.on(
+              'data',
+              function(data){
+                  ipc.log('got a message from world : ', data,data.toString());
+              }
+          );
+      }
+  );
+
+//}
+
+function startServer() {
+  /***************************************\
+   *
+   * You should start both hello and world
+   * then you will see them communicating.
+   *
+   * *************************************/
+
+  // ipc.config.socketRoot = 'C:\\tmp\\';
+  // ipc.config.appspace = 'app.';
+  ipc.config.id = 'world';
+  ipc.config.retry= 1500;
+  ipc.config.rawBuffer=true;
+  ipc.config.encoding='ascii';
+
+  ipc.serve(
+      function(){
+          ipc.server.on(
+              'connect',
+              function(socket){
+                  ipc.server.emit(
+                      socket,
+                      'hello'
+                  );
+              }
+          );
+
+          ipc.server.on(
+              'data',
+              function(data,socket){
+                  ipc.log('got a message', data,data.toString());
+                  ipc.server.emit(
+                      socket,
+                      data.toString()
+                  );
+              }
+          );
+      }
+  );
+
+  ipc.server.start();
+}
